e334aec0440c3b2276d0610f1b2e4ebe99ba2326
rayleighzhongrui <rayleighzhong@gmail.com>
2019-12-31 00:56:58 +0800

    stockpy add industry data

--- stock.py
+++ stock.py
@@ -3,3 +3,4 @@
 from jqdatasdk import *
 auth('18010073691','sunyanzi')
 
+df = finance.run_query(query(finance.SW1_DAILY_PRICE).filter(finance.SW1_DAILY_PRICE.date>='2018-01-01', finance.SW1_DAILY_PRICE.code == '801752'))
